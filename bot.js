84c39688147212829bd03591991a608dfc50b4b1
hamoo22 <44175127+hamoo22@users.noreply.github.com>
2018-10-16 05:11:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -92,18 +92,59 @@ client.on('typingStart', (ch, user) => {
 })
 
 
-client.on('message', message => {
-            var prefix = "م";
-    let args = message.content.split(" ").slice(1);
-if (message.content.startsWith(prefix + 'سح')) {
- let args = message.content.split(" ").slice(1)
-    let messagecount = parseInt(args);
-    if (args > 100) return message.reply("**:octagonal_sign: || يجب ان يكون عدد المسح أقل من 100 .**").then(messages => messages.delete(5000))
-    if (!messagecount) return message.reply("**:bulb: || أختر كميه الرسائل المراد مسحها .**").then(messages => messages.delete(5000))
-    message.channel.fetchMessages({limit: messagecount + 1}).then(messages => message.channel.bulkDelete(messages));
-    message.channel.send(`\`${args}\` : __عدد الرسائل التي تم مسحها __ `).then(messages => messages.delete(5000));
-  }
-  });
+editedcodes.on("message", (edited) => {
+    
+  if (edited.content === '-new') {
+        const reason = edited.content.split(" ").slice(1).join(" ");
+        if (!edited.guild.roles.exists("name", "فريق الدعم")) return edited.channel.send(`**يجب عمل رتبة بأسم \`فريق الدعم\`**`);
+        if (edited.guild.channels.exists("name", "ticket-" + edited.author.id)) return edited.channel.send(`لديك تذكرة من الأسآس :joy:`);
+        edited.guild.createChannel(`just-ticket`, "text").then(c => {
+            let edited1 = edited.guild.roles.find("name", "فريق الدعم");
+            let edited2 = edited.guild.roles.find("name", "@everyone");
+            c.overwritePermissions(edited1, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            c.overwritePermissions(edited2, {
+                SEND_MESSAGES: false,
+                READ_MESSAGES: false
+            });
+            c.overwritePermissions(message.author, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            edited.channel.send(`:white_check_mark: تـم فتح التذكرة , #${c.name}.`);
+            const embed = new Discord.RichEmbed()
+                .setColor(0xCF40FA)
+                .addField(` ${message.author.username}!`, `**مرحبآ    , لدنيا فريق المساعده ليساعدك في أقرب وقت . **`)
+                .setTimestamp();
+            c.send({
+                embed: embed
+            });
+        }).catch(console.error); 
+    }
+    if (edited.content === '-close') {
+        if (!edited.channel.name.startsWith(`ticket-`)) return edited.channel.send(`**لا تستطيع :x:**`);
+
+        edited.channel.send(`**هل انت متأكد ؟ ** `)
+            .then((m) => {
+                edited.channel.awaitMessages(response => response.content === 'نعم', {
+                        max: 1,
+                        time: 10000,
+                        errors: ['time'],
+                    })
+                    .then((collected) => {
+                        edited.channel.delete();
+                    })
+                    .catch(() => {
+                        m.edit('وقت الأغلاق أنتهي , لن تمسح التذكرة .').then(m2 => {
+                            m2.delete();
+                        }, 3000);
+                    });
+            });
+        }
+
+    });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
